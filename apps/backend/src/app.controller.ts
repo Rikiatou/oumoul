7e0cb5f7f55cae89cc333e7326aed483192c543d
KABRAK ENG <kabrakeng@gmail.com>
2026-02-16 13:21:46 +0100

    debug: add email config check endpoint

--- apps/backend/src/app.controller.ts
+++ apps/backend/src/app.controller.ts
@@ -1,12 +1,29 @@
 import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('debug/email-config')
+  getEmailConfig() {
+    const brevoKey = this.configService.get<string>('BREVO_API_KEY');
+    const fromAddr = this.configService.get<string>('EMAIL_FROM_ADDRESS');
+    const fromName = this.configService.get<string>('EMAIL_FROM_NAME');
+    return {
+      brevoKeySet: !!brevoKey,
+      brevoKeyPrefix: brevoKey ? brevoKey.substring(0, 8) + '...' : null,
+      fromAddress: fromAddr ?? 'rikiatouhassansale@gmail.com (default)',
+      fromName: fromName ?? 'Nissa Imane Tracker (default)',
+    };
   }
 }
