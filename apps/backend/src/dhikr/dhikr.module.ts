b6a6c6ee91a1fce8657c629c05727e37b1e49055
KABRAK ENG <kabrakeng@gmail.com>
2026-02-22 00:04:31 +0100

    chore: remove dhikr admin seed endpoint (seed already done)

--- apps/backend/src/dhikr/dhikr.module.ts
+++ apps/backend/src/dhikr/dhikr.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
 import { DhikrService } from './dhikr.service';
 import { DhikrController } from './dhikr.controller';
-import { DhikrAdminController } from './dhikr-admin.controller';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [DhikrController, DhikrAdminController],
+  controllers: [DhikrController],
   providers: [DhikrService],
   exports: [DhikrService],
 })
